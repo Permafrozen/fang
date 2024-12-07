e734f16dd91ed14ed853ea5993e2fe6df84d1624
MeDiamondStar <46242983+MeDiamondStar@users.noreply.github.com>
2024-12-08 00:26:18 +0100

    get_screentofile_command + get_temp_screenshot_file_path

--- fang/src-tauri/src/grab_image.rs
+++ fang/src-tauri/src/grab_image.rs
@@ -1,5 +1,7 @@
 use crate::target_platform::TargetPlatform;
 use which::which;
+use std::{env}; // temp_dir
+use std::path::{Path};
 
 pub fn is_executable_in_path(executable: &str) -> bool {
     which(executable).is_ok()
@@ -18,4 +20,23 @@ pub fn check_availability(platform: TargetPlatform) -> Result<(), String> {
     } else {
         Err(format!("{} not found in PATH", required_executable))
     }
+}
+
+pub fn get_screentofile_command(platform: TargetPlatform, filepath: &str) -> Result<String, String> {
+    let required_executable = match platform {
+        TargetPlatform::Windows => format!("{}{}", "nircmd savescreenshot ", filepath),
+        TargetPlatform::LinuxWayland => format!("{}{}", "grim ", filepath),
+        TargetPlatform::LinuxWaylandGnomeMutter => format!("{}{}", "gnome-screenshot --file=", filepath),
+        _ => return Err("Unsupported platform".to_string()),
+    };
+
+    Ok(required_executable)
+}
+
+pub fn get_temp_screenshot_file_path() -> String {
+    let dir = env::temp_dir();
+    Path::new(dir.as_os_str())
+        .join("fang-tmp-screenshot.png")
+        .to_string_lossy()
+        .into_owned()
 }
