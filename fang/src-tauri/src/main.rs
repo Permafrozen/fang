87574d3592f83258f7aa0055d8f605b69bcf521b
MeDiamondStar <46242983+MeDiamondStar@users.noreply.github.com>
2024-12-07 21:23:38 +0100

    TargetPlatform impl + CLI daemon/image arg handling + listen for PRINT key

--- fang/src-tauri/src/main.rs
+++ fang/src-tauri/src/main.rs
@@ -1,6 +1,83 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// Get help syntax:
+//      cargo run
+// Start just the image editor:
+//      cargo run -- --runas-imgeditor
+// Start as daemon:
+//      cargo run -- --runas-daemon
+
+mod target_platform;
+
+use std::env;
+use rdev::{listen, Event, EventType, Key};
+
+const RUN_AS_DAEMON_ARG: &str = "--runas-daemon";
+const IMAGE_EDITOR_ARG: &str = "--runas-imgeditor";
+
 fn main() {
+    // Fancy printout for version
+    let version = env!("CARGO_PKG_VERSION");
+    println!("Starting FANG {version}");
+
+
+    // get target we're currently running on
+    let target: target_platform::TargetPlatform = target_platform::get_environment();
+    println!("Running on platform \"{:?}\"", target);
+
+
+    // CLI arguments
+    let args: Vec<String> = env::args().collect();
+
+
+    // start different modes depending on how fang was started through CLI
+    if args.contains(&String::from(RUN_AS_DAEMON_ARG)) {
+        
+        start_daemon_loop();
+    
+    }
+    else if args.contains(&String::from(IMAGE_EDITOR_ARG)) {
+        
+        assert!(args.len() == 2, "Insufficient parameters with IMAGE_EDITOR_ARG: path to image as second argument required!");
+
+        open_image_editor(&args[1])
+
+    } else {
+        
+        println!("Insufficient parameters! Syntax: fang <--runas-daemon|--runas-imgeditor [path]>");
+    
+    }
+}
+
+fn start_daemon_loop() {
+    println!("Starting daemon loop");
+
+    // Set up a callback for input
+    let callback = |event: Event| {
+        match event.event_type {
+            EventType::KeyPress(Key::PrintScreen) => {
+                println!("Print screen pressed");
+                capture_image();
+            }
+            _ => {}
+        }
+    };
+
+    // Listen for input indefinite
+    let _ = listen(callback);
+}
+
+fn capture_image() {
+    println!("capturing image...");
+
+    // TODO: let target_platform decide the way it captures the image
+}
+
+fn open_image_editor(path: &str) {
+    println!("Opening image editor with {path} as target image");
+
+    // TODO: pass path argument to fang GUI image editor and let it handle the rest
+
     fang_lib::run()
 }
---
+++ fang/src-tauri/src/target_platform.rs
@@ -0,0 +1,46 @@
+use std::env;
+
+// derive debug in order to be able to print out using {:?} inside println macro
+#[derive(Debug)]
+pub enum TargetPlatform {
+    Unknown,
+    LinuxNonWayland,
+    Windows,
+    LinuxWayland,
+    LinuxWaylandGnomeMutter
+}
+
+fn is_wayland() -> bool {
+    env::var("WAYLAND_DISPLAY").is_ok()
+}
+
+fn is_linux() -> bool {
+    cfg!(target_os = "linux")
+}
+
+fn is_gnome_mutter() -> bool {
+    let session = env::var("XDG_SESSION_DESKTOP").unwrap_or_default();
+    session.to_lowercase().contains("gnome")
+}
+
+fn is_windows() -> bool {
+    cfg!(target_os = "windows")
+}
+
+pub fn get_environment() -> TargetPlatform {
+    if is_linux() {
+        if is_wayland() {
+            if is_gnome_mutter() {
+                crate::target_platform::TargetPlatform::LinuxWaylandGnomeMutter
+            } else {
+                crate::target_platform::TargetPlatform::LinuxWayland
+            }
+        } else {
+            crate::target_platform::TargetPlatform::LinuxNonWayland
+        }
+    } else if is_windows() {
+        crate::target_platform::TargetPlatform::Windows
+    } else {
+        crate::target_platform::TargetPlatform::Unknown
+    }
+}
