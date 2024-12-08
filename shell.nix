dd0c5510f898200c72d02777b92f2ad10c78ab14
Permafrozen <m8tt8o@gmail.com>
2024-12-08 11:51:35 +0100

    Keine Ahnung ob das ein fix ist, aber ich geb auf.

--- shell.nix
+++ shell.nix
@@ -25,7 +25,37 @@ in pkgs.mkShell {
     openssl
   ];
 
-  shellHook = ''
-    export WEBKIT_DISABLE_DMABUF_RENDERER="1"
+  XDG_DATA_DIRS = let
+    base = pkgs.lib.concatMapStringsSep ":" (x: "${x}/share") [
+      pkgs.adwaita-icon-theme
+      pkgs.shared-mime-info
+    ];
+    gsettings_schema = pkgs.lib.concatMapStringsSep ":"
+      (x: "${x}/share/gsettings-schemas/${x.name}") [
+        pkgs.glib
+        pkgs.gsettings-desktop-schemas
+        pkgs.gtk3
+      ];
+  in "${base}:${gsettings_schema}";
+
+  preFixup = ''
+    gappsWrapperArgs+=(
+      --set WEBKIT_DISABLE_COMPOSITING_MODE 1
+      --prefix XDG_DATA_DIRS : ${
+        pkgs.lib.concatMapStringsSep ":" (x: "${x}/share") [
+          pkgs.adwaita-icon-theme
+          pkgs.shared-mime-info
+        ]
+      }
+      --prefix XDG_DATA_DIRS : ${
+        pkgs.lib.concatMapStringsSep ":"
+        (x: "${x}/share/gsettings-schemas/${x.name}") [
+          pkgs.glib
+          pkgs.gsettings-desktop-schemas
+          pkgs.gtk3
+        ]
+      }
+      --prefix GIO_EXTRA_MODULES : ${pkgs.glib-networking}/lib/gio/modules
+    )
   '';
 }
